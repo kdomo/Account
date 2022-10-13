27461c6de52097f1005f97358bfb0f3a179e89c9
kdomo <dongho5088@naver.com>
2022-10-13 14:38:11 +0900

    feat : redisson를 통한 Lock 추가

--- src/main/java/com/domo/account/controller/AccountController.java
+++ src/main/java/com/domo/account/controller/AccountController.java
@@ -1,12 +1,20 @@
 package com.domo.account.controller;
 
 import com.domo.account.service.AccountService;
+import com.domo.account.service.RedisServiceTest;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class AccountController {
     private final AccountService accountService;
+    private final RedisServiceTest redisServiceTest;
 
+    @GetMapping("get-lock")
+    public String getLock(){
+        return redisServiceTest.getLock();
+    }
 }
