0b619454e225f704c13d580fdee0e0833504f21e
kdomo <dongho5088@naver.com>
2022-10-13 17:49:17 +0900

    test : verify, assertThrows 테스트 사용해보기

--- src/test/java/com/domo/account/service/AccountServiceTest.java
+++ src/test/java/com/domo/account/service/AccountServiceTest.java
@@ -6,6 +6,7 @@ import com.domo.account.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,8 +14,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
@@ -27,19 +31,39 @@ class AccountServiceTest {
 
     @Test
     @DisplayName("계좌 조회 성공")
-    void testXXX() {
+    void findAccount_success() {
         //given
         given(accountRepository.findById(anyLong()))
                 .willReturn(Optional.of(Account.builder()
                         .accountStatus(AccountStatus.UNREGISTERED)
                         .accountNumber("65789")
                         .build()));
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
 
         //when
         Account account = accountService.getAccount(4L);
 
         //then
+        verify(accountRepository, times(1)).findById(captor.capture());
+        verify(accountRepository, times(0)).save(any());
+        assertEquals(4L, captor.getValue());
         assertEquals("65789", account.getAccountNumber());
         assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+
+//        assertNotEquals();
+//        assertTrue();
+//        assertThrows();
+    }
+
+    @Test
+    @DisplayName("계좌 조회 실패 - 음수")
+    void findAccount_fail() {
+        //given
+
+        //when
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.getAccount(-10L));
+
+        //then
+        assertEquals("Minus", exception.getMessage());
     }
 }
